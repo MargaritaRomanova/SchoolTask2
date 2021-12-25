ed50985501d62cb64d4e2426264085a5c9de05c7
MargaritaRomanova <steklova.margarita@yandex.ru>
2021-12-25 22:56:39 +0300

    change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,13 +5,13 @@ import static model.Calculator.execute;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Input the first value");
-        String one = in.nextLine();
-        System.out.println("Input the second value");
-        String two = in.nextLine();
-        System.out.println("Input sing");
+       // System.out.println("Input the first value");
         String sign = in.nextLine();
+       // System.out.println("Input the second value");
+        String one = in.nextLine();
+       // System.out.println("Input sing");
+        String two = in.nextLine();
         String[] params = new String[]{sign, one, two};
-        System.out.println("Answer: " + execute(params));
+        System.out.println(execute(params));
     }
 }
